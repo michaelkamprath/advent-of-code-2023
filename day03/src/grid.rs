35279f2c4b9884def302640f585123f3ed81e806
Michael Kamprath <michael@kamprath.net>
2023-12-02 23:35:29 -0800

    formatting

--- day03/src/grid.rs
+++ day03/src/grid.rs
@@ -1,154 +1,138 @@
-
 use std::fmt::Debug;
 
 pub trait GridEntity: Debug {
-	fn location(&self) -> Location;
+    fn location(&self) -> Location;
 
-	fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool;
+    fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool;
 }
 
-#[derive(Debug,Copy,Clone,Eq,PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Location {
-	x: u32,
-	y: u32,
+    x: u32,
+    y: u32,
 }
 
 impl Location {
-	pub fn new(x: u32, y: u32) -> Self {
-		Self {
-			x,
-			y,
-		}
-	}
+    pub fn new(x: u32, y: u32) -> Self {
+        Self { x, y }
+    }
 }
-
 
 impl GridEntity for Location {
-	fn location(&self) -> Location {
-		*self
-	}
+    fn location(&self) -> Location {
+        *self
+    }
 
-	fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool
-	{
-		let other_loc = other.location();
+    fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool {
+        let other_loc = other.location();
 
-		// adjacent if X or Y is +/-1
-		let x_start = if other_loc.x == 0 { 0 } else { other_loc.x - 1 };
-		let y_start = if other_loc.y == 0 { 0 } else { other_loc.y - 1 };
+        // adjacent if X or Y is +/-1
+        let x_start = if other_loc.x == 0 { 0 } else { other_loc.x - 1 };
+        let y_start = if other_loc.y == 0 { 0 } else { other_loc.y - 1 };
 
-		(x_start..other_loc.x+2).contains(&self.x) && (y_start..other_loc.y+2).contains(&self.y)
-	}
+        (x_start..other_loc.x + 2).contains(&self.x) && (y_start..other_loc.y + 2).contains(&self.y)
+    }
 }
 
-
-
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct Value {
-	locations: Vec<Location>,
-	value: u32,
+    locations: Vec<Location>,
+    value: u32,
 }
 
 impl Value {
-	pub fn new(start_location: Location, num_str: &str) -> Self {
-		let mut locations: Vec<Location> = Vec::new();
+    pub fn new(start_location: Location, num_str: &str) -> Self {
+        let mut locations: Vec<Location> = Vec::new();
 
-		locations.push(start_location);
-		for i in 1..num_str.len() {
-			locations.push(Location::new(start_location.x + i as u32, start_location.y));
-		}
+        locations.push(start_location);
+        for i in 1..num_str.len() {
+            locations.push(Location::new(start_location.x + i as u32, start_location.y));
+        }
 
-		let value = match num_str.parse() {
-			Ok(v) => v,
-			Err(e) => {
-				panic!("could not parse string: {:?}, error = {}", num_str, e);
-			}
-		};
+        let value = match num_str.parse() {
+            Ok(v) => v,
+            Err(e) => {
+                panic!("could not parse string: {:?}, error = {}", num_str, e);
+            }
+        };
 
-		Self {
-			locations,
-			value,
-		}
-	}
+        Self { locations, value }
+    }
 
-	pub  fn value(&self) -> u32 {
-		self.value
-	}
-
+    pub fn value(&self) -> u32 {
+        self.value
+    }
 }
 
 impl GridEntity for Value {
-	fn location(&self) -> Location {
-		self.locations[0]
-	}
+    fn location(&self) -> Location {
+        self.locations[0]
+    }
 
-	fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool {
-		self.locations.iter().any( |&loc| loc.is_adjacent(other))
-	}	
+    fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool {
+        self.locations.iter().any(|&loc| loc.is_adjacent(other))
+    }
 }
-
 
 #[derive(Debug, Clone, Copy)]
 pub struct Symbol {
-	location: Location,
-	symbol: char,
+    location: Location,
+    symbol: char,
 }
 
 impl Symbol {
-	pub fn new(location: Location, symbol: char) -> Self {
-		Self {
-			location,
-			symbol,
-		}
-	}
+    pub fn new(location: Location, symbol: char) -> Self {
+        Self { location, symbol }
+    }
 
-	pub fn symbol(&self) -> char {
-		self.symbol
-	}
+    pub fn symbol(&self) -> char {
+        self.symbol
+    }
 }
 
 impl GridEntity for Symbol {
-	fn location(&self) -> Location {
-		self.location
-	}
+    fn location(&self) -> Location {
+        self.location
+    }
 
-	fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool {
-		other.is_adjacent(&self.location)
-	}
+    fn is_adjacent(&self, other: &(dyn GridEntity)) -> bool {
+        other.is_adjacent(&self.location)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{GridEntity, Location, Value, Symbol};
+    use super::{GridEntity, Location, Symbol, Value};
 
     #[test]
     fn test_location() {
-    	let l1 = Location::new(2, 6);
-    	let l2 = Location::new(4, 8);
-    	let l3 = Location::new(3, 7);
-    	let l4 = Location::new(1, 1);
-    	let l5 = Location::new(0, 0);
+        let l1 = Location::new(2, 6);
+        let l2 = Location::new(4, 8);
+        let l3 = Location::new(3, 7);
+        let l4 = Location::new(1, 1);
+        let l5 = Location::new(0, 0);
 
-    	assert!(!l1.is_adjacent(&l2));
-    	assert!(l2.is_adjacent(&l3));
-    	assert!(l3.is_adjacent(&l2));
-    	assert!(l1.is_adjacent(&l3));
-    	assert!(!l1.is_adjacent(&l4));
-    	assert!(l4.is_adjacent(&l5));
+        assert!(!l1.is_adjacent(&l2));
+        assert!(l2.is_adjacent(&l3));
+        assert!(l3.is_adjacent(&l2));
+        assert!(l1.is_adjacent(&l3));
+        assert!(!l1.is_adjacent(&l4));
+        assert!(l4.is_adjacent(&l5));
     }
 
     #[test]
     fn test_value() {
-    	let v1 = Value::new(Location::new(2,5), "247");
+        let v1 = Value::new(Location::new(2, 5), "247");
 
-    	assert_eq!(v1.value(), 247);
-    	assert!(v1.is_adjacent(&Location::new(5,6)));
-    	assert!(!v1.is_adjacent(&Location::new(1,1)));
+        assert_eq!(v1.value(), 247);
+        assert!(v1.is_adjacent(&Location::new(5, 6)));
+        assert!(!v1.is_adjacent(&Location::new(1, 1)));
     }
 
     #[test]
     fn test_symbol() {
-    	let s1 = Symbol::new(Location::new(3,3), '#');
+        let s1 = Symbol::new(Location::new(3, 3), '#');
 
-    	assert!(s1.is_adjacent(&Location::new(2,2)));
+        assert!(s1.is_adjacent(&Location::new(2, 2)));
     }
- }
+}
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,5 @@
 mod grid;
 
-
 use std::io;
 
 use grid::{GridEntity, Location, Symbol, Value};
@@ -13,61 +12,70 @@ fn main() -> io::Result<()> {
     let mut values: Vec<Value> = Vec::new();
     let mut symbols: Vec<Symbol> = Vec::new();
 
-     while let Some(Ok(line)) = lines.next() {
+    while let Some(Ok(line)) = lines.next() {
         let mut i: usize = 0;
         let line_chars: Vec<char> = line.chars().collect();
         while i < line.len() {
             match line_chars[i] {
                 '.' => {
                     i += 1;
-                },
+                }
                 '0'..='9' => {
                     // scan ahead to find first non digit or end
-                    let num_str: String = line_chars[i..].iter().take_while(|c| c.is_digit(10)).map(|c| *c).collect();
+                    let num_str: String = line_chars[i..]
+                        .iter()
+                        .take_while(|c| c.is_digit(10))
+                        .map(|c| *c)
+                        .collect();
 
                     values.push(Value::new(Location::new(i as u32, y), num_str.as_str()));
 
                     i += num_str.len();
-
-                },
+                }
                 _ => {
                     symbols.push(Symbol::new(Location::new(i as u32, y), line_chars[i]));
                     i += 1;
-                },
-
+                }
             }
-
         }
 
         y += 1;
-     }
+    }
 
-     // Part 1
-     let mut value_sum = 0;
+    // Part 1
+    let mut value_sum = 0;
 
-     for v in &values {
+    for v in &values {
         if symbols.iter().any(|s| v.is_adjacent(s)) {
             value_sum += v.value();
         }
-     }
+    }
 
-     println!("part 1 value sum = {:?}", value_sum);
+    println!("part 1 value sum = {:?}", value_sum);
 
-     // part 2
+    // part 2
 
-    let star_symbols: Vec<Symbol> = symbols.iter().filter(|s| s.symbol() == '*').map(|s| *s).collect();
+    let star_symbols: Vec<Symbol> = symbols
+        .iter()
+        .filter(|s| s.symbol() == '*')
+        .map(|s| *s)
+        .collect();
 
     let mut gear_ratio_sum: u32 = 0;
 
     for s in &star_symbols {
-        let adjacent_values: Vec<Value> = values.iter().filter(|v| v.is_adjacent(s)).map(|v| v.clone()).collect();
+        let adjacent_values: Vec<Value> = values
+            .iter()
+            .filter(|v| v.is_adjacent(s))
+            .map(|v| v.clone())
+            .collect();
 
         if adjacent_values.len() == 2 {
-            gear_ratio_sum += adjacent_values[0].value()*adjacent_values[1].value();
+            gear_ratio_sum += adjacent_values[0].value() * adjacent_values[1].value();
         }
     }
 
     println!("part 2 gear ratio sum = {:?}", gear_ratio_sum);
-     
-     Ok(())
+
+    Ok(())
 }
